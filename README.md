bfa92cabc45d7f0e8a0d17bd33e6443cf5af9c34
Ziga Bobnar <ziga@bobnar.net>
2025-04-24 13:23:50 +0200

    hardhat config

--- README.md
+++ README.md
@@ -34,3 +34,15 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+## Deploy the smart contracts on blockchain
+
+Start the local test network:
+
+`npx hardhat node`
+
+Deploy contracts:
+
+`npx hardhat compile`
+
+`npx hardhat run blockchain/scripts/deploy.js --network localhost`
---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
+
+module.exports = {
+  solidity: "0.8.29",
+  paths: {
+    artifacts: "./blockchain/artifacts",
+    sources: "./blockchain/contracts",
+    cache: "./blockchain/cache",
+    tests: "./blockchain/test"
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
